67d89039619c17287a2bde4cbffff79404f33a0e
Kиро.Kрика <95271587+Goshko812@users.noreply.github.com>
2024-12-12 11:37:00 +0200

    added world server timer

--- setup.sh
+++ setup.sh
@@ -132,7 +132,18 @@ Restart=always
 [Install]
 WantedBy=multi-user.target" | sudo tee /etc/systemd/system/mangosd.service
 
-    sudo systemctl daemon-reload
+   echo "[Unit]
+Description=Restart Mangosd every 5 hours
+
+[Timer]
+OnBootSec=5h
+OnUnitActiveSec=5h
+
+[Install]
+WantedBy=timers.target" | sudo tee /etc/systemd/system/mangosd.timer
+
+   sudo systemctl daemon-reload
+   
 }
 
 extract_game_data() {
@@ -189,7 +200,7 @@ main() {
     echo "CMangos setup completed successfully!
     Please remember to change Console.Enable line in your mangosd.conf file after creating an account to ensure the systemd service works properly:
     This setting must be updated to 0 to disable the console for the service to function correctly. Until then, you can keep the console enabled for account creation.
-    After Console.Enable is set to 0 you can do systemctl enable realmd and systemctl enable mangosd"
+    After Console.Enable is set to 0 you can do systemctl enable realmd and systemctl enable mangosd and optionally enable and start the mangosd.timer which will restart the game world every 5 hours"
 }
 
 main "$@"
